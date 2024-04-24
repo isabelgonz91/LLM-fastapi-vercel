d82e43f6f901c88206ca84e3214f547becd27382
isabelgonz91 <36759536+isabelgonz91@users.noreply.github.com>
2024-04-24 14:50:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Asegúrate de incluir el dominio correcto aquí
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 
