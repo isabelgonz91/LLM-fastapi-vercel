7b80d76afc3235dce53e534e6fca9f4f881dfdd1
isabelgonz91 <36759536+isabelgonz91@users.noreply.github.com>
2024-04-24 10:03:23 +0200

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:" \
+                          f"{os.environ['DATABASE_PASSWORD']}@" \
+                          f"{os.environ['DATABASE_HOST']}:" \
+                          f"{os.environ['DATABASE_PORT']}/" \
+                          f"{os.environ['DATABASE_NAME']}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
