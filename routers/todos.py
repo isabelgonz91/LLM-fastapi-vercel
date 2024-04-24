5c47e535f5b04e9b5964fb34b0049cfa8abc1f54
isabelgonz91 <36759536+isabelgonz91@users.noreply.github.com>
2024-04-24 13:21:02 +0200

    Update todos.py

--- routers/todos.py
+++ routers/todos.py
@@ -3,10 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
 import schemas
 import crud
-from fastapi.responses import Response
 from database import SessionLocal
-from langchain import OpenAI, PromptTemplate
-from langchain.chains import LLMChain
 
 router = APIRouter(
     prefix="/todos"
@@ -48,51 +45,3 @@ def delete_todo(id: int, db: Session = Depends(get_db)):
     res = crud.delete_todo(db, id)
     if res is None:
         raise HTTPException(status_code=404, detail="to do not found")
-
-# LANGCHAIN
-langchain_llm = OpenAI(temperature=0)
-
-summarize_template_string = """
-        Provide a summary for the following text:
-        {text}
-"""
-
-summarize_prompt = PromptTemplate(
-    template=summarize_template_string,
-    input_variables=['text'],
-)
-
-summarize_chain = LLMChain(
-    llm=langchain_llm,
-    prompt=summarize_prompt,
-)
-
-@router.post('/summarize-text')
-async def summarize_text(text: str):
-    summary = summarize_chain.run(text=text)
-    return {'summary': summary}
-
-write_poem_template_string = """
-        Write a short poem with the following text:
-        {text}
-"""
-
-write_poem_prompt = PromptTemplate(
-    template=write_poem_template_string,
-    input_variables=['text'],
-)
-
-write_poem_chain = LLMChain(
-    llm=langchain_llm,
-    prompt=write_poem_prompt,
-)
-
-@router.post("/write-poem/{id}")
-async def write_poem_for_todo_id(id: int, db: Session = Depends(get_db)):
-    todo = crud.read_todo(db, id)
-    if todo is None:
-        raise HTTPException(status_code=404, detail="to do not found")
-    poem = write_poem_chain.run(text=todo.name)
-    return {'poem': poem}
-
-
