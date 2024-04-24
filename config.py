503600567986b331befc486281188544ad747b01
isabelgonz91 <36759536+isabelgonz91@users.noreply.github.com>
2024-04-24 10:00:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-     DATABASE_HOST: str
-     DATABASE_NAME: str
-     DATABASE_USER: str
-     DATABASE_PASSWORD: str
-     DATABASE_PORT: int
-     app_name: str = "Full Stack To Do App"
+    DATABASE_HOST: str
+    DATABASE_NAME: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_PORT: int
+    app_name: str = "Full Stack To Do App"
 
-     class Config:
-         env_file = ".env"
-         extra = "ignore"
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+        extra = "ignore"
+
+# Crear una instancia de Settings que será usada en toda la aplicación
+settings = Settings()
